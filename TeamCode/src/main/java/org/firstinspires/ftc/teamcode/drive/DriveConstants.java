8d0609374cc1d3afe8fa1ea26f3613dfef0d4f8b
Farhad Ibrahimzade <67722444+farhadnbps@users.noreply.github.com>
2021-02-04 15:09:36 -0500

    Tuning the Robot to implement Road Runner

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
@@ -34,8 +34,7 @@ public class DriveConstants {
      * from DriveVelocityPIDTuner.
      */
     public static final boolean RUN_USING_ENCODER = true;
-    public static PIDFCoefficients MOTOR_VELO_PID = new PIDFCoefficients(0, 0, 0,
-      getMotorVelocityF(MAX_RPM / 60 * TICKS_PER_REV));
+    public static PIDFCoefficients MOTOR_VELO_PID = new PIDFCoefficients(30, 2, 8, 12);
 
     /*
      * These are physical constants that can be determined from your robot (including the track
@@ -46,7 +45,7 @@ public class DriveConstants {
      * convenience. Make sure to exclude any gear ratio included in MOTOR_CONFIG from GEAR_RATIO.
      */
     public static double WHEEL_RADIUS = 2; // in
-    public static double GEAR_RATIO = 1; // output (wheel) speed / input (motor) speed
+    public static double GEAR_RATIO = 0.97; // output (wheel) speed / input (motor) speed
     public static double TRACK_WIDTH = 12; // in
 
     /*
